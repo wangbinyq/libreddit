1106b0d6ec65c2b28c0ffc222a5e1930628fc181
wangb <wangbinyq@gmail.com>
2023-01-29 16:59:49 +0800

    update formdata

--- src/settings.rs
+++ src/settings.rs
@@ -59,7 +59,8 @@ pub async fn set(req: Request) -> Result<Response, String> {
 	let mut response = redirect("/settings".to_string());
 
 	for &name in &PREFS {
-		match form.get_all(name).get(1).as_string() {
+		let data = form.get_all(name);
+		match data.get(data.length() - 1).as_string() {
 			Some(value) => response.insert_cookie(
 				Cookie::build(name.to_owned(), value.clone())
 					.path("/")
